2908a864cefda4fa00d6c565edc1ad4f76dd0017
rvohmin <rvohmin@iflex.ru>
2021-03-23 17:51:50 +0300

    CSS - приведено к макету, добавлены модальные окна

--- src/main/java/ru/vohmin/springboot/control/AdminController.java
+++ src/main/java/ru/vohmin/springboot/control/AdminController.java
@@ -2,15 +2,18 @@ package ru.vohmin.springboot.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.vohmin.springboot.model.Role;
 import ru.vohmin.springboot.model.User;
 import ru.vohmin.springboot.repository.RoleRepository;
 import ru.vohmin.springboot.service.UserService;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
@@ -18,8 +21,8 @@ import java.security.Principal;
 public class AdminController {
     private final UserService service;
     private final RoleRepository roleRepository;
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     public AdminController(UserService service, RoleRepository roleRepository) {
         this.service = service;
@@ -29,6 +32,8 @@ public class AdminController {
     @GetMapping(value = "/users")
     public String getUsers(ModelMap model, Principal principal) {
         model.addAttribute("authUser", getAuthorizedUser(principal));
+        model.addAttribute("allRoles", roleRepository.findAll());
+        model.addAttribute("user", new User());
         model.addAttribute("users", service.getUsers());
         return "users";
     }
@@ -43,14 +48,14 @@ public class AdminController {
 
     @PostMapping("/add_user")
     public String addUser(@ModelAttribute User user, @RequestParam("roles") String[] rolesFromHtml) {
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         service.saveUser(user, rolesFromHtml);
         return "redirect:/admin/users";
     }
 
-    @PostMapping("/del_user/{id}")
-    public String delUser(@PathVariable @Validated Long id) {
-        service.deleteUser(id);
+    @PostMapping("/del_user/")
+    public String delUser(@ModelAttribute User user) {
+        service.deleteUser(user.getId());
         return "redirect:/admin/users";
     }
 
@@ -58,9 +63,20 @@ public class AdminController {
     public String redirectToMergePage(@PathVariable @Validated Long id, ModelMap map) {
         map.addAttribute("user", service.findUserById(id));
         map.addAttribute("allRoles", roleRepository.findAll());
-        return "update";
+        return "redirect:/admin/users";
     }
 
+    @GetMapping("/roles")
+    public String getRoles() {
+        List<Role> all = roleRepository.findAll();
+        StringBuilder sb = new StringBuilder();
+        sb.append("{[");
+        for (Role role : all) {
+            sb.append(role.getName()).append(" ");
+        }
+        sb.append("]}");
+        return sb.toString();
+    }
     @PostMapping("/update/merge")
     public String updateUser(@ModelAttribute User user) {
         service.updateUser(user);
@@ -68,7 +84,6 @@ public class AdminController {
     }
 
     private User getAuthorizedUser(Principal principal) {
-//        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return service.findByUsername(principal.getName());
     }
 }
